1ba10cc01cef73c8215c7af0d3c3a617b7a1173d
Unknown <Sam.Davidsson86@outlook.com>
2017-10-25 22:02:13 +0200

    Autofac and viewmodels
    Added some viewmodel, set up autofac and made the gui

--- FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using FriendOrganizer.UI.StartUp;
 using System.Windows;
 
 namespace FriendOrganizer.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new BootStrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            //ToDo: Load data from database.
+            yield return new Friend { FirstName = "Kenopp", LastName = "Alic" };
+            yield return new Friend { FirstName = "Jacob", LastName = "Gan" };
+            yield return new Friend { FirstName = "Jacob", LastName = "Demo" };
+            yield return new Friend { FirstName = "Samwise", LastName = "Gamgi" };
+        }
+    }
+}
